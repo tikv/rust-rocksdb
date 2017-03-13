956b86333dd69016691cb9f9d7be43eda38b3c4a
Dylan.Wen <hhkbp2@gmail.com>
2017-03-13 10:06:12 +0800

    address comment

--- librocksdb_sys/crocksdb/c.cc
+++ librocksdb_sys/crocksdb/c.cc
@@ -1060,9 +1060,10 @@ crocksdb_options_t* crocksdb_get_options(const crocksdb_t* db) {
 }
 
 crocksdb_options_t* crocksdb_get_options_cf(
-    const crocksdb_t* db, crocksdb_column_family_handle_t* column_family) {
+    const crocksdb_t* db,
+    crocksdb_column_family_handle_t* column_family) {
   crocksdb_options_t* options = new crocksdb_options_t;
-  options-> rep = db->rep->GetOptions();
+  options-> rep = db->rep->GetOptions(column_family->rep);
   return options;
 }
 

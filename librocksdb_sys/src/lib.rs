2a6ecdc365b1d9a5aa8ad162238d227b2e2242a5
Huachao Huang <huachao.huang@gmail.com>
2018-06-30 12:50:52 +0800

    *: add bottommost level compaction
    Expose the bottommost level compaction option to make it more flexible.

--- librocksdb_sys/src/lib.rs
+++ librocksdb_sys/src/lib.rs
@@ -292,6 +292,14 @@ pub enum DBTableProperty {
     CompressionName = 17,
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(C)]
+pub enum DBBottommostLevelCompaction {
+    Skip = 0,
+    IfHaveCompactionFilter = 1,
+    Force = 2,
+}
+
 pub fn error_message(ptr: *mut c_char) -> String {
     let c_str = unsafe { CStr::from_ptr(ptr) };
     let s = format!("{}", c_str.to_string_lossy());
@@ -1021,6 +1029,10 @@ extern "C" {
     pub fn crocksdb_compactoptions_set_change_level(opt: *mut DBCompactOptions, v: bool);
     pub fn crocksdb_compactoptions_set_target_level(opt: *mut DBCompactOptions, v: i32);
     pub fn crocksdb_compactoptions_set_max_subcompactions(opt: *mut DBCompactOptions, v: i32);
+    pub fn crocksdb_compactoptions_set_bottommost_level_compaction(
+        opt: *mut DBCompactOptions,
+        v: DBBottommostLevelCompaction,
+    );
 
     pub fn crocksdb_fifo_compaction_options_create() -> *mut DBFifoCompactionOptions;
     pub fn crocksdb_fifo_compaction_options_set_max_table_files_size(

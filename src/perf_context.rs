e9190103eae955ad16870779098d1ebb881a7d62
Huachao Huang <huachao.huang@gmail.com>
2018-12-17 16:27:10 +0800

    *: format

--- src/perf_context.rs
+++ src/perf_context.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crocksdb_ffi::{self, DBPerfContext, DBIOStatsContext};
+use crocksdb_ffi::{self, DBIOStatsContext, DBPerfContext};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum PerfLevel {

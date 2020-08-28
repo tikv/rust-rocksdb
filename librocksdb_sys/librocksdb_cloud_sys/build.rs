dd9ebd0868a8ba64315ad3da0c38edb26adcc768
Devdutt Shenoi <devdutt@outlook.in>
2020-08-28 22:57:57 +0530

    aws: move build/linking into librocksdb subcrate
    Signed-off-by: Devdutt Shenoi <devdutt@outlook.in>

--- librocksdb_sys/librocksdb_cloud_sys/build.rs
+++ librocksdb_sys/librocksdb_cloud_sys/build.rs
@@ -4,7 +4,26 @@ extern crate cmake;
 
 use std::env;
 
-fn main() {
+fn load_aws_sdk() {
+    let dst = cmake::Config::new("aws-sdk-cpp")
+        .define("BUILD_ONLY", "kinesis;core;s3;transfer")
+        .define("CMAKE_BUILD_TYPE", "RelWithDebInfo")
+        .define("ENABLE_TESTING", "OFF")
+        .define("STATIC_LINKING", "1")
+        .very_verbose(true)
+        .build();
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=aws-c-common");
+    println!("cargo:rustc-link-lib=static=aws-checksums");
+    println!("cargo:rustc-link-lib=static=aws-c-event-stream");
+    println!("cargo:rustc-link-lib=static=aws-cpp-sdk-core");
+    println!("cargo:rustc-link-lib=static=aws-cpp-sdk-kinesis");
+    println!("cargo:rustc-link-lib=static=aws-cpp-sdk-s3");
+    println!("cargo:rustc-link-lib=static=aws-cpp-sdk-transfer");
+}
+
+fn load_rocksdb_cloud() {
     let cur_dir = env::current_dir().unwrap();
     let mut cfg = cmake::Config::new(".");
     let dst = cfg
@@ -12,6 +31,12 @@ fn main() {
         .build_target("rocksdb_cloud")
         .very_verbose(true)
         .build();
+
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=rocksdb_cloud");
+}
+
+fn main() {
+    load_aws_sdk();
+    load_rocksdb_cloud();
 }

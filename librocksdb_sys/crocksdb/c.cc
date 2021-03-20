967484551b15a85c8e1096e9aeb8708a14051c66
DingC <1017472599@qq.com>
2021-03-20 09:37:50 +0800

    fix compile bug

--- librocksdb_sys/crocksdb/c.cc
+++ librocksdb_sys/crocksdb/c.cc
@@ -6276,13 +6276,11 @@ void crocksdb_level_region_accessor_result_destory(crocksdb_level_region_accesso
 
 unsigned char crocksdb_level_region_accessor_result_equal(crocksdb_level_region_accessor_result_t* result1,
                                                  crocksdb_level_region_accessor_result_t* result2) {
-  regions_1 = result1->rep.regions;
-  regions_2 = result2->rep.regions;
-  if (regions_1.size() != region_2.size()) return 0;
+  if (result1->rep.regions.size() != result2->rep.regions.size()) return 0;
   else {
-    for (int i = 0; i < regions_1.size(); ++i) {
-      if (regions_1[i].smallest_user_key.Compare(regions_2[i].smallest_user_key) != 0 ||
-          regions_1[i].largest_user_key.Compare(regions_2[i].largest_user_key) != 0) {
+    for (int i = 0; i < result1->rep.regions.size(); ++i) {
+      if (result1->rep.regions[i].smallest_user_key.Compare(result2->rep.regions[i].smallest_user_key) != 0 ||
+          result1->rep.regions[i].largest_user_key.Compare(result2->rep.regions[i].largest_user_key) != 0) {
         return 0;
       }
     }

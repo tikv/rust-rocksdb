5411724d35937b6b9c874df5404e7148ad51f2e9
Jianjun Liao <jianjun.liao@outlook.com>
2026-06-01 20:50:34 +0800

    commit some suggestions
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- librocksdb_sys/crocksdb/crocksdb/c.h
+++ librocksdb_sys/crocksdb/crocksdb/c.h
@@ -2023,7 +2023,7 @@ crocksdb_slicetransform_create(
     unsigned char (*in_range)(void*, const char* key, size_t length),
     const char* (*name)(void*));
 extern C_ROCKSDB_LIBRARY_API crocksdb_slicetransform_t*
-    crocksdb_slicetransform_create_fixed_prefix(size_t);
+crocksdb_slicetransform_create_fixed_prefix(size_t);
 extern C_ROCKSDB_LIBRARY_API crocksdb_slicetransform_t*
 crocksdb_slicetransform_create_noop();
 extern C_ROCKSDB_LIBRARY_API void crocksdb_slicetransform_destroy(
